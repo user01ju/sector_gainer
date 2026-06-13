960f2a6d1e65334ea22942c747405deef735cec7
user01ju <user01ju@users.noreply.github.com>
2026-06-13 20:09:38 +0800

    Daily update (manual)

--- update_report.bat
+++ update_report.bat
@@ -25,6 +25,7 @@ git add -A
 git diff --cached --quiet
 if not errorlevel 1 (
   echo No changes to commit.
+  pause
 ) else (
   git commit -m "Daily update (manual)"
   git push
@@ -34,6 +35,7 @@ if not errorlevel 1 (
   )
 )
 exit /b 0
+
 
 :fail
 echo.
